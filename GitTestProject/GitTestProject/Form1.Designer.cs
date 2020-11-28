e1844b47d37dab87d6f56bbddeb44365a1cbeec8
Vladimir Liberman <vvlib@yahoo.com>
2020-11-28 14:45:35 -0600

    Version 3.0.403.2

--- GitTestProject/GitTestProject/Form1.Designer.cs
+++ GitTestProject/GitTestProject/Form1.Designer.cs
@@ -29,14 +29,26 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Location = new System.Drawing.Point(320, 76);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(282, 149);
             this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -46,6 +58,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -53,6 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
