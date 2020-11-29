e1efce987ffadbac2771aceceec7618020d5555c
Vladimir Liberman <vvlib@yahoo.com>
2020-11-29 15:16:00 -0600

    Test Commit

--- GitTestProject/GitTestProject/Form1.Designer.cs
+++ GitTestProject/GitTestProject/Form1.Designer.cs
@@ -36,9 +36,9 @@
             // panel1
             // 
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(320, 76);
+            this.panel1.Location = new System.Drawing.Point(320, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(282, 149);
+            this.panel1.Size = new System.Drawing.Size(282, 213);
             this.panel1.TabIndex = 0;
             // 
             // button1
